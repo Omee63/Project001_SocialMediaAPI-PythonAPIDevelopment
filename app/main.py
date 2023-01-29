0d04a2c176835a03fb03ea37b0d3448954a46f83
Omee63 <shariaromee63@gmail.com>
2023-01-29 19:36:38 +0600

    Some code changes

--- app/main.py
+++ app/main.py
@@ -42,7 +42,7 @@ def get_posts():
 
 
 @app.post("/posts",
-          status_code=status.HTTP_201_CREATED)  # status code have to be selected according to HTTP response status code Documentaiton
+          status_code=status.HTTP_201_CREATED)  #status code have to be selected according to HTTP response status code Documentaiton
 def create_post(post: Post):
     post_dict = post.dict()
     post_dict['id'] = randrange(0, 1000000)
@@ -69,3 +69,16 @@ def delete_post(id: int):
 
     my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    index = find_index_post(id)
+
+    if index is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} doesn't exist.")
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+
+    return {"data": post_dict}
